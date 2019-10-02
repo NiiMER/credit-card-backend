39562372fc32476df93247436c6bc58a846c4f5b
Mohamad Nimer <m7ammad.nimer@gmail.com>
2019-10-02 15:31:36 +0300

    Adding JSDocs to the firebase app manager

--- src/interface/firebaseManager.mjs
+++ src/interface/firebaseManager.mjs
@@ -3,24 +3,50 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const { apiKey, databaseURL, projectId } = config.get("firebase.config");
-
 let firebaseApp, db;
 
+/**
+ * @object {Object} firebaseManager object to provide operational functions for firebase
+ */
 const firebaseManager = {
+  /**
+   * Function to initialize the firebase application
+   */
   initFirebase: () => {
     firebaseApp = firebase.initializeApp({ apiKey, databaseURL, projectId });
     db = firebase.database();
   },
-  addCreditCardDetails: () =>
-    db.ref("credit-card-storage/1").set({
-      cardName: "test1",
-      cardNumber: 111111111111111,
-      limit: 10
+  /**
+   * Function to add cards to the database.
+   * @param cardDetail {object} Object to insert to the database by fields
+   * @param cardDetail.cardName {object} Card holder name
+   * @param cardDetail.cardNumber {object} Card Number
+   * @param cardDetail.limit {object} Card limit amount
+   */
+  addCreditCardDetails: (
+    cardDetail = {
+      cardName: "",
+      cardNumber: "",
+      limit: 0
+    }
+  ) =>
+    db.ref(`credit-card-storage/${new Date(Date.now()).getTime()}`).set({
+      cardName,
+      cardNumber,
+      limit
     }),
+  /**
+   * Function to get the cards.
+   */
   getCreditCardDetails: async () => {
     const snapshot = await db.ref("credit-card-storage").once("value");
     return snapshot.val() || [];
-  }
+  },
+  /**
+   * Function to get the firebaseApp instance
+   * @returns {object} The instance of the firebaseApp
+   */
+  getfirebaseApp: () => firebaseApp
 };
 
 export default firebaseManager;
