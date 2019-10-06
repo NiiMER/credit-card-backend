de1df760c79aa1dc2416d58c4401435e44d2a5b1
Mohamad Nimer <m7ammad.nimer@gmail.com>
2019-10-06 10:16:19 +0300

    Adding more test cases for credit cards

--- src/api/mop/tests/mop.middleware.test.js
+++ src/api/mop/tests/mop.middleware.test.js
@@ -1,18 +1,61 @@
 import * as mopMiddleware from "../mop.middleware";
 
 describe("Validating Credit Card", () => {
-  it("Try validating credit card with 0", () => {
+  it("FAILING: Validating credit card with 0", () => {
     const validationResult = mopMiddleware.validateCard("0");
     expect(validationResult).toEqual(false);
   });
 
-  it("Try validating credit card with 5 Numbers ", () => {
+  it("FAILING: Validating credit card with 5 Numbers ", () => {
     const validationResult = mopMiddleware.validateCard("11111");
     expect(validationResult).toEqual(false);
   });
 
-  it("Try validating VISA credit card", () => {
+  it("SUCCESS: Validating VISA credit card", () => {
     const validationResult = mopMiddleware.validateCard("4270539990719306");
     expect(validationResult).toEqual(true);
+  });
+
+  it("FAILING: Validating VISA credit card with more than 19 characters", () => {
+    const validationResult = mopMiddleware.validateCard(
+      "4270539990719306990719306"
+    );
+    expect(validationResult).toEqual(false);
+  });
+
+  it("SUCCESS: Validating MasterCard credit card", () => {
+    const validationResult = mopMiddleware.validateCard("5458756487121736");
+    expect(validationResult).toEqual(true);
+  });
+
+  it("SUCCESSFAILING: Validating MasterCard credit card with more than 19 characters", () => {
+    const validationResult = mopMiddleware.validateCard(
+      "54587564871217362173621736"
+    );
+    expect(validationResult).toEqual(false);
+  });
+
+  it("SUCCESS: Validating American Express credit card", () => {
+    const validationResult = mopMiddleware.validateCard("373941884747852");
+    expect(validationResult).toEqual(true);
+  });
+
+  it("FAILING: Validating American Express credit card", () => {
+    const validationResult = mopMiddleware.validateCard(
+      "37394188474785284747852"
+    );
+    expect(validationResult).toEqual(false);
+  });
+
+  it("SUCCESS: Validating Visa Electron credit card", () => {
+    const validationResult = mopMiddleware.validateCard("4917245371663696");
+    expect(validationResult).toEqual(true);
+  });
+
+  it("FAILING: Validating Visa Electron credit card", () => {
+    const validationResult = mopMiddleware.validateCard(
+      "491724537166369671663696"
+    );
+    expect(validationResult).toEqual(false);
   });
 });
